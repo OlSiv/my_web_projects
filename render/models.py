ac0388d172ae0f59a5c55e334185057fd62a7edd
Oleg Siv <pomo24abc@gmail.com>
2024-05-07 18:30:12 +0300

    for run seed.py_2

--- render/models.py
+++ render/models.py
@@ -5,25 +5,25 @@
 #   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
-# from django.db import models
+from django.db import models
 
 
-# class AuthUser(models.Model):
-#     id = models.IntegerField(primary_key=True)
-#     password = models.CharField(max_length=128)
-#     last_login = models.DateTimeField(blank=True, null=True)
-#     is_superuser = models.BooleanField()
-#     username = models.CharField(unique=True, max_length=150)
-#     first_name = models.CharField(max_length=150)
-#     last_name = models.CharField(max_length=150)
-#     email = models.CharField(max_length=254)
-#     is_staff = models.BooleanField()
-#     is_active = models.BooleanField()
-#     date_joined = models.DateTimeField()
+class AuthUser(models.Model):
+    id = models.IntegerField(primary_key=True)
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
 
-#     class Meta:
-#         managed = False
-#         db_table = 'auth_user'
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
 
 
 # class AuthGroup(models.Model):
@@ -130,8 +130,8 @@
 ###
 
 
-# class Message(models.Model):
-#     text_message = models.CharField(max_length=512)
-#     image = models.FilePathField()
-#     message_time = models.DateTimeField()
-#     user = models.ForeignKey(AuthUser, on_delete=models.CASCADE, null=True)
+class Message(models.Model):
+    text_message = models.CharField(max_length=512)
+    image = models.FilePathField()
+    message_time = models.DateTimeField()
+    user = models.ForeignKey(AuthUser, on_delete=models.CASCADE, null=True)
