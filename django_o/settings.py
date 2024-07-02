017b864aaa0e1fa451163c04bb849db2e9a0a207
Oleg Siv <pomo24abc@gmail.com>
2024-07-03 01:03:59 +0300

    02_07_2024_03

--- django_o/settings.py
+++ django_o/settings.py
@@ -156,10 +156,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATICFILES_DIRS = [
-	os.path.join(BASE_DIR, 'render/static'),
+	os.path.join(BASE_DIR, 'render/static/'),
 ]
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- render/views.py
+++ render/views.py
@@ -8,13 +8,13 @@ def index(request):
     mess_m = Message.objects.all()[::-1]
     users_names = User.objects.all()
 
-    context = {
-        'mess1': mess_m, 
-        'users_names': users_names,
-               }
+    context = {'mess1': mess_m, 
+               'users_names': users_names}
 ###
-    return render(request, 'render/index.html', 
-                  context=context
-                  )
+    return render(request, 
+                  'render/index.html', 
+                  context=context)
+
+
 
     # return HttpResponse('Hello') 
