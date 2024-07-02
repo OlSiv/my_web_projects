2729118a3168f087d20463dcc8157b67a2773456
Oleg Siv <pomo24abc@gmail.com>
2024-07-02 18:21:56 +0300

    02_07_2024_01

--- render/views.py
+++ render/views.py
@@ -1,21 +1,23 @@
 from django.shortcuts import render
 from .models import Message
 from django.contrib.auth.models import User 
+from django.http import HttpResponse
 
 
 def index(request):
 
-    mess_m = Message.objects.all()[::-1]
-    users_names = User.objects.all()
+#     mess_m = Message.objects.all()[::-1]
+#     users_names = User.objects.all()
 
-    context = {
-        'mess1': mess_m, 
-        'users_names': users_names,
+#     context = {
+#         'mess1': mess_m, 
+#         'users_names': users_names,
 
-               }
-###
-    return render(request, 'render/index.html', 
-                  context=context
-                  )
+#                }
+# ###
+#     return render(request, 'render/index.html', 
+#                   context=context
+#                   )
 
+    return HttpResponse('Hello') 
 
