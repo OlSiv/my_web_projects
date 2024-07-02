d96dd554ff5d0a2ad6f44c4b20720a282492de82
Oleg Siv <pomo24abc@gmail.com>
2024-07-02 23:16:56 +0300

    02_07_2024_02

--- django_o/settings.py
+++ django_o/settings.py
@@ -73,7 +73,7 @@ ROOT_URLCONF = 'django_o.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [], # <<<<<<<<<<<<<<<<<<
+        'DIRS': ['templates'], # <<<<<<<<<<<<<<<<<<
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- render/views.py
+++ render/views.py
@@ -5,19 +5,16 @@ from django.http import HttpResponse
 
 
 def index(request):
+    mess_m = Message.objects.all()[::-1]
+    users_names = User.objects.all()
 
-#     mess_m = Message.objects.all()[::-1]
-#     users_names = User.objects.all()
+    context = {
+        'mess1': mess_m, 
+        'users_names': users_names,
+               }
+###
+    return render(request, 'render/index.html', 
+                  context=context
+                  )
 
-#     context = {
-#         'mess1': mess_m, 
-#         'users_names': users_names,
-
-#                }
-# ###
-#     return render(request, 'render/index.html', 
-#                   context=context
-#                   )
-
-    return HttpResponse('Hello') 
-
+    # return HttpResponse('Hello') 
